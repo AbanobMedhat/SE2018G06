9bc942386b0d3651d00ac3934432d0b2e8b2683c
Abanob Medhat <abanobmedhat42@gmail.com>
2019-02-14 01:38:29 +0200

    Back End version 1.3

--- Website/Back End/EN/Electronic/models.py
+++ Website/Back End/EN/Electronic/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, User
 
 
 class CustomUser(AbstractUser):
@@ -10,17 +10,16 @@ class CustomUser(AbstractUser):
 
 
 class Product(models.Model):
-    class Product(models.Model):
-        User = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-        type = models.CharField(max_length=250)
-        title = models.CharField(max_length=350)
-        cpu = models.CharField(max_length=250)
-        processor = models.CharField(max_length=250)
-        ram = models.CharField(max_length=150)
-        storage = models.CharField(max_length=150)
-        color = models.CharField(max_length=150)
-        battery = models.CharField(max_length=150)
-        price = models.CharField(max_length=150)
+    User = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    type = models.CharField(max_length=250)
+    title = models.CharField(max_length=350)
+    cpu = models.CharField(max_length=250)
+    processor = models.CharField(max_length=250)
+    ram = models.CharField(max_length=150)
+    storage = models.CharField(max_length=150)
+    color = models.CharField(max_length=150)
+    battery = models.CharField(max_length=150)
+    price = models.CharField(max_length=150)
 
-        def __str__(self):
-            return self.title
+    def __str__(self):
+        return self.title
